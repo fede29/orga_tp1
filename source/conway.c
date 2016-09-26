c417062b787ba0c5d0747f3eaefd0c8c20743a4c
pabloascarza <pabloqac87@gmail.com>
2016-09-26 20:50:33 -0300

    se fixean parametros

--- source/conway.c
+++ source/conway.c
@@ -52,7 +52,7 @@ unsigned int vecinos (unsigned char *a, unsigned int i, unsigned int j, unsigned
 void loadBoard (char* fileName, unsigned char **board ) {
 	printf("start loading the file \n");
 	FILE* file = fopen(fileName, "r");
-	char line[3];
+	char line[2];
 
 	while (fgets(line, sizeof(line), file)) {
 		printf("reading line \n");
@@ -70,11 +70,16 @@ void loadBoard (char* fileName, unsigned char **board ) {
 
 int main(int argc, char* argv[]) {
 	printf("start \n");
-	char* fileName = argv[3];
-	unsigned int rows = atoi(argv[1]);
-	unsigned int cols = atoi(argv[2]);
-	unsigned int actionsCount = atoi(argv[0]);
+	char* fileName = argv[4];
+	unsigned int rows = atoi(argv[2]);
+	unsigned int cols = atoi(argv[3]);
+	unsigned int actionsCount = atoi(argv[1]);
 	printf("end loading parameters \n");
+
+	printf("parameter 1 %i \n", actionsCount);
+	printf("parameter 2 %i \n", cols);
+	printf("parameter 3 %i \n", rows);
+	printf("parameter 4 %s \n", fileName);
 
 	printf("init the variables \n");
 	unsigned int i = 0;
